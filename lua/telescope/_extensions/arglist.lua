efc2b58ee4514b2d5a4b04ba6032ff7f75a2a44d
Sam Ellis <samwilliamellis@gmail.com>
2022-08-10 22:51:58 +0100

    chore: check the user has a compatible Neovim version

--- lua/telescope/_extensions/arglist.lua
+++ lua/telescope/_extensions/arglist.lua
@@ -1,4 +1,10 @@
--- Check if required dependencies are installed.
+-- Check if the user has the required Neovim version.
+if vim.fn.has('nvim-0.7.0') ~= 1 then
+  vim.api.nvim_err_writeln('The plugin `telescope-arglist.nvim` requires Neovim v0.7.0 or higher.')
+  return
+end
+
+-- Check if the user has the required dependencies installed.
 local has_telescope, telescope = pcall(require, 'telescope')
 local has_plenary, plenary = pcall(require, 'plenary')
 
@@ -12,8 +18,9 @@ if not has_plenary then
   return
 end
 
-return require('telescope').register_extension {
+-- Register the extension.
+return require('telescope').register_extension({
   exports = {
     arglist = require('telescope-arglist').arglist,
   },
-}
+})
